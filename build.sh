1b1d66ee749f2487e38b7e8a62065000c7a931b0
itsnandhu2004 <nandhini.info2004@gmail.com>
2025-02-05 14:25:01 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-echo "🔨 Starting Build Script..."
-
-# Stop if any command fails
-set -e
-
-echo "hi123"
-
-# Build the Docker image
 docker build -t test .
 
 echo "✅ Build Completed!"
