eb43dd7c27be032a9a931d197d81bfdc3a76c0da
itsnandhu2004 <nandhini.info2004@gmail.com>
2025-02-05 14:50:38 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 echo run
 chmod 777 build.sh
 ./build.sh
-docker login -u nandhini1694 -p dckr_pat_iwWSgQJXmZBj65X-KQqiXNjVFMM
+docker login -u nandhini1694 -p dckr_pat_08XhH0G6oTntx5pCb0hPNQVtQ1k
 docker tag test nandhini1694/myrepo
 docker push nandhini1694/myrepo
 
