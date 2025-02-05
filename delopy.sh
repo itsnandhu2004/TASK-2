4e2030487fb02e79d05a6aede6a30bafb083ac5e
itsnandhu2004 <nandhini.info2004@gmail.com>
2025-02-05 14:41:29 +0530

    Update delopy.sh

--- delopy.sh
+++ delopy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-docker login -u -p
-docker tag 
-docker push
+chmod 777 build.sh
+./build.sh
+docker login -u nandhini1694 -p dckr_pat_iwWSgQJXmZBj65X-KQqiXNjVFMM
+docker tag test nandhini1694/myrepo
+docker push nandhini1694/myrepo
 
